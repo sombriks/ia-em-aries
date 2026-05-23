1b4fdcf10324ed0b94da617b3ecb65611aa02049
Leonardo Silveira <sombriks@gmail.com>
2026-05-23 12:41:52 -0300

    explorando

--- .eleventy.js
+++ .eleventy.js
@@ -9,12 +9,13 @@ export default function (config) {
   config.setLayoutsDirectory("../components");
   config.setLayoutsDirectory("../layouts");
   config.setOutputDirectory('./dist');
+
   config.addPlugin(EleventyVitePlugin, {
     viteOptions: {
       server: {
         mode: "development",
         middlewareMode: true,
-        watch: ["./src/scripts/**"]
+        watch: ["./src/scripts/**"] // not working
       },
       build: {
         rolldownOptions: {
@@ -22,7 +23,7 @@ export default function (config) {
             main: "./src/scripts/index.js"
           },
           output: {
-            entryFileNames: "assets/[name].js"
+            entryFileNames: "assets/[name].js" // not refreshing
           },
         }
       }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,30 @@
-// teste
-console.log("o bagulho é doido mesmo");
+import { CreateMLCEngine } from "@mlc-ai/web-llm";
+
+console.log("carrega");
+
+async function main() {
+  // Callback function to update model loading progress
+  const initProgressCallback = (initProgress) => {
+    console.log(initProgress);
+  };
+
+  const selectedModel = "Llama-3.1-8B-Instruct-q4f32_1-MLC";
+
+  const engine = await CreateMLCEngine(
+    selectedModel,
+    { initProgressCallback: initProgressCallback }, // engineConfig
+  );
+
+  const messages = [
+    { role: "system", content: "You are a helpful AI assistant." },
+    { role: "user", content: "Hello!" },
+  ];
+
+  const reply = await engine.chat.completions.create({
+    messages,
+  });
+  console.log(reply.choices[0].message);
+  console.log(reply.usage);
+}
+
+main();
